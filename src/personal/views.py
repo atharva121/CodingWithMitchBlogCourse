d2207feb7f129507b6dabc0fc6ee35eaef4870f5
atharva121 <atharvasaraf@gmail.com>
2019-08-30 23:47:32 +0530

    3rd commit

---
+++ src/personal/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home_screen_view(request):
+    context = {}
+    #context['some_string'] = "this is some string"
+    #context['some_number'] = 213213
+    #context = {
+    #    'some_string': "this is some string",
+    #    'some_number': 12312312
+    #}
+    list_of_values = []
+    list_of_values.append("first entry")
+    list_of_values.append("second entry")
+    list_of_values.append("third entry")
+    list_of_values.append("fourth entry")
+    context['list_of_values'] = list_of_values
+    return render(request, "personal/home.html", context)
