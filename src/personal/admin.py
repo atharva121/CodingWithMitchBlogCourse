101dddc10564df8b3e65a86247a0255c91364b00
atharva121 <atharvasaraf@gmail.com>
2019-09-03 00:20:20 +0530

    6th commit

--- src/personal/admin.py
+++ src/personal/admin.py
@@ -1,5 +1,1 @@
 from django.contrib import admin
-from account.models import Account
-
-# Register your models here.
-admin.site.register(Account)
--- src/personal/views.py
+++ src/personal/views.py
@@ -1,21 +1,9 @@
 from django.shortcuts import render
-from personal.models import Question
+from account.models import Account
 
 # Create your views here.
 def home_screen_view(request):
     context = {}
-    #context['some_string'] = "this is some string"
-    #context['some_number'] = 213213
-    #context = {
-    #    'some_string': "this is some string",
-    #    'some_number': 12312312
-    #}
-    #list_of_values = []
-    #list_of_values.append("first entry")
-    #list_of_values.append("second entry")
-    #list_of_values.append("third entry")
-    #list_of_values.append("fourth entry")
-    #context['list_of_values'] = list_of_values
-    questions = Question.objects.all()
-    context["questions"] = questions
+    accounts = Account.object.all()
+    context['accounts'] = accounts
     return render(request, "personal/home.html", context)
