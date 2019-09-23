2e07d177807d9ae5a89ab6df66d5f79b788ea8f5
atharva121 <atharvasaraf@gmail.com>
2019-09-24 00:04:27 +0530

    22nd commit

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Q
+from django.http import HttpResponse
 from blog.models import BlogPost
 from blog.forms import CreateBlogPostForm, UpdateBlogPostForm
 from account.models import Account
@@ -34,6 +35,8 @@ def edit_blog_view(request, slug):
     if not user.is_authenticated:
         return redirect('must_authenticate')
     blog_post = get_object_or_404(BlogPost, slug=slug)
+    if blog_post.author != user:
+        return HttpResponse("You are not the author of that post.")
     if request.POST:
         form = UpdateBlogPostForm(request.POST or None, request.FILES or None, instance=blog_post)
         if form.is_valid():
